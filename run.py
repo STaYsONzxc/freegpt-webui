b42da1829300eafbafdc9f5577b0650db900ff53
STaYsON <80705216+STaYsONzxc@users.noreply.github.com>
2023-09-11 00:17:38 +0700

    Update run.py

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from server.bp import bp
 from server.website import Website
 from server.backend import Backend_Api
 from server.babel import create_babel
+from pyngrok import ngrok  # Импортируйте ngrok
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Run the FreeGPT web application")
@@ -21,14 +22,14 @@ if __name__ == '__main__':
     site_config = config['site_config']
     url_prefix = config.pop('url_prefix')
 
-    # Create the app
+    # Создайте экземпляр приложения Flask
     app = Flask(__name__)
     app.secret_key = secrets.token_hex(16)
 
-    # Set up Babel
+    # Настройте Babel
     create_babel(app)
 
-    # Set up the website routes
+    # Настройте маршруты для веб-сайта
     site = Website(bp, url_prefix)
     for route in site.routes:
         bp.add_url_rule(
@@ -37,7 +38,7 @@ if __name__ == '__main__':
             methods=site.routes[route]['methods'],
         )
 
-    # Set up the backend API routes
+    # Настройте маршруты для бэкенда API
     backend_api = Backend_Api(bp, config)
     for route in backend_api.routes:
         bp.add_url_rule(
@@ -46,14 +47,19 @@ if __name__ == '__main__':
             methods=backend_api.routes[route]['methods'],
         )
 
-    # Register the blueprint
+    # Зарегистрируйте blueprint
     app.register_blueprint(bp, url_prefix=url_prefix)
 
-    # Run Flask with Ngrok
-    run_with_ngrok(app)
+    # Запуск Flask с использованием Ngrok
+    if args.token:
+        # Используйте токен Ngrok, если он предоставлен в аргументах командной строки
+        ngrok.set_auth_token(args.token)
 
-    # Удалите 'host' из site_config, так как Flask уже настроен для использования Ngrok
-    site_config.pop('host', None)
+    # Запустите Ngrok и получите общедоступный URL
+    public_url = ngrok.connect(port=site_config['port'])
+
+    # Выведите общедоступный URL в консоль
+    print(" * Ngrok URL:", public_url)
 
     # Замените `app.run(**site_config)` на простой запуск Flask
     app.run()
